55415950b8201041b1005c2edeae4d933505001f
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-05-06 11:57:28 -0500

    Create a custom hook for useQuery using tanstack react query.

--- src/features/orders/useOrders.ts
+++ src/features/orders/useOrders.ts
@@ -1,3 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
 import { fetchDailyOrders } from './ordersApi';
+
 export const useDailyOrders = () => {
+  return useQuery({
+    queryKey: ['dailyOrders'], // A unique key for caching
+    queryFn: fetchDailyOrders,
+    staleTime: 1000 * 60 * 5,  // Data stays fresh for 5 minutes (no refetching if user changes tabs)
+  });
 };
