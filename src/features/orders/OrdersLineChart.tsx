9d0595e7ca91fa1e608c8e9a324c0eb94926a3f9
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-05-07 17:47:45 -0500

    Base file for a Plotly Presenter Component of the Orders line chart.

---
+++ src/features/orders/OrdersLineChart.tsx
@@ -0,0 +1,14 @@
+// src/features/orders/OrdersLineChart.tsx
+import Plot from 'react-plotly.js';
+import { DailyOrdersResponse } from '../../types/api';
+
+interface Props {
+  data: DailyOrdersResponse;
+}
+
+export default function OrdersLineChart({ data }: Props) {
+  return (
+	// TODO 
+	// Tailwind styling for the chart container
+  );
+}
