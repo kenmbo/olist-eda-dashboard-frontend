e5f10783e0d53fe65bd329213a24b454077e4786
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-05-12 17:03:32 -0500

    Implement tanstack query to the useCategorySales Data Fetcher hook.

--- src/features/categories/useCategorySales.ts
+++ src/features/categories/useCategorySales.ts
@@ -1,12 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
 import type { CategorySalesResponse } from '../../types/api';
 
 const fetchCategorySales = async (): Promise<CategorySalesResponse> => {
-  const response = 0;
+  const response = await fetch(`https://localhbost:8000/api/categories/sales`);
   if (!response.ok) throw new Error('Network response was not ok');
   return response.json();
 };
 
-
 export const useCategorySales = () => {
-   return 0;
-}
+  return useQuery({
+    queryKey: ['categorySales'],
+    queryFn: fetchCategorySales,
+    staleTime: 1000 * 60 * 5,
+  });
+};
