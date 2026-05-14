158509e9886aba183e16c27de307330cef88cc4f
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-05-13 20:41:32 -0500

    Create Treemap presenter component: CategoryTreemap.tsx.

---
+++ src/features/categories/CategoryTreemap.tsx
@@ -0,0 +1,9 @@
+import type { CategorySalesResponse } from '../../types/api';
+
+interface Props {
+  data: CategorySalesResponse;
+}
+
+export default function CategoryTreemap({ data }: Props) {
+   return 0;
+}
