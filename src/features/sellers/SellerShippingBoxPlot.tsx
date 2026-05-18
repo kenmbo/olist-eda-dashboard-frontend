5bd1c5f6baaeaaf56b543c6c229c7ab4edbbb72a
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-05-18 18:11:18 -0500

    feat: Create Box Plot, presenter, SellerShippingBoxPlot.tsx.

---
+++ src/features/sellers/SellerShippingBoxPlot.tsx
@@ -0,0 +1,9 @@
+import type { SellerShippingResponse } from '../../types/api';
+
+interface Props {
+  data: SellerShippingResponse;
+}
+
+export default function SellerShippingBoxPlot({ data }: Props) {
+  return None;
+}
