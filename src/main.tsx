fc60ce25734c0f7aea8be82a904284add5b338e5
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-07-03 15:40:08 -0500

    fix: resolve TS6133, remove unused `React` and `ReactDOM` imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import { StrictMode } from 'react'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -9,11 +7,11 @@ import App from './App.tsx'
 // Create a client
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     {/* Provide the client to your App */}
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
